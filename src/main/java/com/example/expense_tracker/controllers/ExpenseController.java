0d3c5db602ab52f29dbc5218302b83a21d6283ed
Nicole Vicente <nicolevs94@gmail.com>
2025-01-28 19:39:12 +0100

    Add new controller with Get all, Get detail, Create and update + Advice for Exception

--- src/main/java/com/example/expense_tracker/controllers/ExpenseController.java
+++ src/main/java/com/example/expense_tracker/controllers/ExpenseController.java
@@ -1,35 +1,86 @@
 package com.example.expense_tracker.controllers;
 
-import java.util.Collection;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.expense_tracker.dtos.ExpenseDto;
 import com.example.expense_tracker.entities.Expense;
+import com.example.expense_tracker.exceptions.ExpenseNotFoundException;
 import com.example.expense_tracker.repositories.ExpenseRepository;
+import com.example.expense_tracker.services.ExpenseService;
 
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/expense")
+@RequestMapping("/expenses")
 public class ExpenseController {
+    
+    private ExpenseRepository expenseRepository;
+    private ExpenseService expenseService;
 
-    private final ExpenseRepository expenseRepository;
-
-    public ExpenseController(ExpenseRepository expenseRepository) {
+    public ExpenseController(ExpenseRepository expenseRepository, ExpenseService expenseService) {
         this.expenseRepository = expenseRepository;
+        this.expenseService = expenseService;
     }
     
+     /**
+     * Retrieves all expenses with pagination.
+     *
+     * @param page The page number (starting from 0).
+     * @param size The number of items per page.
+     * @return A paginated response with expense details.
+     */
     @GetMapping
-    public Collection<Expense> getExpenses() {
-        return expenseRepository.findAll();
+    public Page<Expense> getExpenses(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+        ) {
+        PageRequest pageable = PageRequest.of(page, size);
+        return expenseRepository.findAll(pageable);
     }
 
+    /**
+     * Retrieves a specific expense by its ID.
+     *
+     * @param id The ID of the expense to retrieve (from the path).
+     * @return A JSON response with the expense details or an error message.
+     */
+    @GetMapping("{id}")
+    public Expense getExpense(@PathVariable Long id) {
+        return expenseRepository.findById(id)
+            .orElseThrow(() -> new ExpenseNotFoundException(id));
+    }
+
+    /**
+     * Creates a new expense.
+     *
+     * @param expenseRequest The request body containing the details of the expense to create.
+     *                       Must be valid according to the defined constraints.
+     * @return The created expense object.     
+     */
     @PostMapping
-    public Expense createExpense(@Valid @RequestBody Expense expense) {
-        return expenseRepository.save(expense);
+    public ExpenseDto createExpense(@Valid @RequestBody ExpenseDto expenseInput) {
+        return expenseService.createExpense(expenseInput);
+    }
+
+    /**
+     * Updates a new expense.
+     *
+     * @param id The ID of the expense to update (from the path).
+     * @param expenseRequest The request body containing the details of the expense to update.
+     *                       Must be valid according to the defined constraints.
+     * @return The created expense object.     
+     */
+    @PutMapping("{id}")
+    public ExpenseDto updatExpense(@PathVariable Long id, @Valid @RequestBody ExpenseDto expenseInput) {
+        return expenseService.updateExpense(id, expenseInput);
     }
 }
---
+++ src/main/java/com/example/expense_tracker/exceptions/ExpenseNotFoundAdvice.java
@@ -0,0 +1,23 @@
+package com.example.expense_tracker.exceptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExpenseNotFoundAdvice {
+    
+    @ExceptionHandler(ExpenseNotFoundException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, Object> expenseNotFoundHandler(ExpenseNotFoundException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", ex.getMessage());
+        response.put("error", "Not Found");
+        response.put("status", HttpStatus.BAD_REQUEST.value());
+        return response;
+    }
+}
